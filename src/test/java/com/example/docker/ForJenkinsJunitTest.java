77d3cb10cbd4b7797945cac0e30fbb44c23abcba
LeeTangle <adszzz11@naver.com>
2022-10-26 14:30:50 +0900

    asdf

---
+++ src/test/java/com/example/docker/ForJenkinsJunitTest.java
@@ -0,0 +1,23 @@
+package com.example.docker;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class ForJenkinsJunitTest {
+
+    @Test
+    public void alwaysTrue() {
+        assertThat(1).isEqualTo(1);
+    }
+
+    @Test
+    public void alwaysFalse() {
+        assertThat(2).isEqualTo(1);
+
+    }
+
+}
